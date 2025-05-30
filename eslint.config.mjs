c7b84d73c031f370c995db0fae567ef01cb1a9d6
Jp Santana <jpsantana003@gmail.com>
2025-05-30 18:19:56 -0300

    feat: bugs

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import tseslint from 'typescript-eslint';
+
+export default [
+  { 
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    ignores: ['**/node_modules/**', '**/dist/**', '**/build/**']
+  },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/eol-last': 'error',
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/linebreak-style': ['error', 'unix'],
+      '@stylistic/max-len': ['error', { code: 120 }],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': 'error',
+      'sort-imports': 'error',
+    },
+  },
+];
